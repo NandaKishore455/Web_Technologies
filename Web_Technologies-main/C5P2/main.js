4fcf96db7a3357439f18ca0e26a34cbd59818a0b
NandaKishore455 <23311a05w5@cse.sreenidhi.edu.in>
2026-03-26 15:34:06 +0530

    Add files via upload

---
+++ Web_Technologies-main/C5P2/main.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config(); 
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.log("Error connecting to MongoDB:", err));
+
+
+app.use('/files', require('./routes/FileRoutes'));
+
+// Start server
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
